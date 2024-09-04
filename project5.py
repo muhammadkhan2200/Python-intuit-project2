712868872cfcf50d49c15de4659e9026fabbb3e8
muhammadkhan2200 <168642564+muhammadkhan2200@users.noreply.github.com>
2024-09-04 14:21:44 -0700

    Delete project5.py

--- project5.py
+++
@@ -1,7 +0,0 @@
-
-# Define the function
-def greet_user(khan):
-     # Print the greeting message
-    print(f"Hello, {'khan'} Welcome to Python Functions.")
-    # Call the function with a username
-    greet_user("khan ")
